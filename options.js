548f398eb48971b957ff4b1dcd9bb192189289ad
Dillon DuPont <ddupont@mit.edu>
2022-01-20 20:04:37 -0500

    more fixes

--- options.js
+++ options.js
@@ -11,37 +11,43 @@ function save_options() {
   fetchNotes(({ epoch } = data) => {
     var backgroundColor = document.getElementById('backgroundColor').value;
     var foregroundColor = document.getElementById('foregroundColor').value;
-    chrome.storage.sync.set({
+    chrome.storage.local.set({
       "epoch": epoch + 1,
-      "backgroundColor" : backgroundColor,
-      "foregroundColor" : foregroundColor,
-    }, function() {
-      // Update status to let user know options were saved.
-      var status = document.getElementById('status');
-      status.textContent = 'Options saved.';
-      setTimeout(function() {
-        status.textContent = '';
-      }, 750);
+      "backgroundColor": backgroundColor,
+      "foregroundColor": foregroundColor,
+    }, function () {
+      chrome.storage.sync.set({
+        "epoch": epoch + 1,
+        "backgroundColor": backgroundColor,
+        "foregroundColor": foregroundColor,
+      }, function () {
+        // Update status to let user know options were saved.
+        var status = document.getElementById('status');
+        status.textContent = 'Options saved.';
+        setTimeout(function () {
+          status.textContent = '';
+        }, 750);
+      });
     });
   });
 }
 
 function default_val(val, def) {
-  if(val == "[object Object]")  return def;
-  else                          return val;
+  if (val == "[object Object]") return def;
+  else return val;
 }
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
   chrome.storage.sync.get({
-    "backgroundColor" : backgroundColor,
-    "foregroundColor" : foregroundColor,
-  }, function(items) {
-    document.getElementById('backgroundColor').value  = default_val(items.backgroundColor, "#35363A");
-    document.getElementById('foregroundColor').value  = default_val(items.foregroundColor, "#DADCE0");
+    "backgroundColor": backgroundColor,
+    "foregroundColor": foregroundColor,
+  }, function (items) {
+    document.getElementById('backgroundColor').value = default_val(items.backgroundColor, "#35363A");
+    document.getElementById('foregroundColor').value = default_val(items.foregroundColor, "#DADCE0");
   });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click',
-    save_options);
+  save_options);
