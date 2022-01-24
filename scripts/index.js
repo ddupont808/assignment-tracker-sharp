7b0202517c55a7ee52fb4167f9e8c633ffed2709
Dillon DuPont <ddupont@mit.edu>
2022-01-23 23:54:51 -0500

    v1.0.4 - fixed bug where changes would revert if you close the tab too quickly

--- scripts/index.js
+++ scripts/index.js
@@ -51,6 +51,9 @@ var quill = new Quill('#editor', {
 new QuillMarkdown(quill);
 
 function getUpdatedNotes({ epoch, notes, foregroundColor, backgroundColor }) {
+    if (currnetEpoch < (epoch || 0))
+        return;
+    
     $('html, body, .editor-container, #editor').css('backgroundColor', backgroundColor || '#35363A');
     $('html, body, .editor-container, #editor').css('color', foregroundColor || '#DADCE0');
 
